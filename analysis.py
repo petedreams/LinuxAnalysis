d0acedbd31636e0252e1ffafe5fa88b2eda3c1ea
petedreams <koukaku95@hotmail.com>
2014-06-06 20:45:36 +0900

    second

--- analysis.py
+++ analysis.py
@@ -7,6 +7,7 @@
 
 import subprocess,paramiko,sys,time
 
+#manager
 HOST = '192.168.100.50'
 USER = 'iouser'
 PASS = '8ik,.lo9'
@@ -17,27 +18,31 @@ try:
 except:
     print "./analysis [検体パス(managerの)] [実行時間]"
 
-#vm起動
+#vm終了・SS復元・起動
 subprocess.Popen(["VBoxManage","controlvm","guest-ubuntu10.04","poweroff"]).wait()
 subprocess.Popen(["VBoxManage","snapshot","guest-ubuntu10.04","restore","GUEST"]).wait()
 subprocess.call(["VBoxManage","startvm","guest-ubuntu10.04"])
 
+#SSHでマネージャ接続
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect(HOST,username=USER,password=PASS)
 command = "echo koidekun |sudo -S ./analysis_manager.py %s %s" % (time,filepath)
 ssh.exec_command(command)
-ssh.close()
 
-
-#強制終了
+#Ctrl-Cで強制終了 count秒で正常終了
 count = time + 30
 
 print "強制終了 は Hit Ctrl-C"
 try:
     for i in range(0,count):
-        sleep(1) 
+        sleep(1) # could use a backward counter to be preeety :)
     print('正常終了しました。')
 except KeyboardInterrupt:
     raw_input('終了します Hit Enter:')
-    print('強制終了しました。')
+#
+#   ここで終了処理
+#
+   print('強制終了しました。')
+
+ssh.close()
