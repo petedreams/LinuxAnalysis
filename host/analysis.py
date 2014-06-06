dab0c015851ff1c44a1a83dfc70b3192f5ad48eb
petedreams <koukaku95@hotmail.com>
2014-06-07 00:05:53 +0900

    modify

--- host/analysis.py
+++ host/analysis.py
@@ -11,38 +11,48 @@ import subprocess,paramiko,sys,time
 HOST = '192.168.100.50'
 USER = 'iouser'
 PASS = '8ik,.lo9'
+SCRIPT_PATH  = '/home/iouser/analysis/analysis_manager.py'
 
-try:
-    filepath = sys.argv[1]
-    exetime = sys.argv[2]
-except:
-    print "./analysis [検体パス(managerの)] [実行時間]"
+def sshconnect(host,user,pswd):
+    obj = paramiko.SSHClient()
+    obj.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    obj.connect(host,username=user,password=pswd)
+    return obj
 
-#vm終了・SS復元・起動
-subprocess.Popen(["VBoxManage","controlvm","guest-ubuntu10.04","poweroff"]).wait()
-subprocess.Popen(["VBoxManage","snapshot","guest-ubuntu10.04","restore","GUEST"]).wait()
-subprocess.call(["VBoxManage","startvm","guest-ubuntu10.04"])
+if __name__ == '__main__':
 
-#SSHでマネージャ接続
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(HOST,username=USER,password=PASS)
-command = "echo koidekun |sudo -S ./analysis_manager.py %s %s" % (time,filepath)
-ssh.exec_command(command)
+    try:
+        filepath = sys.argv[1]
+        exetime = sys.argv[2]
+    except:
+        print "./analysis [検体パス(managerの)] [実行時間]"
+    
+    #vm終了・SS復元・起動
+    subprocess.Popen(["VBoxManage","controlvm","guest-ubuntu10.04","poweroff"]).wait()
+    subprocess.Popen(["VBoxManage","snapshot","guest-ubuntu10.04","restore","GUEST"]).wait()
+    subprocess.call(["VBoxManage","startvm","guest-ubuntu10.04"])
+    
+    #guestのネット接続待ち
+    time.sleep(10)
 
-#Ctrl-Cで強制終了 count秒で正常終了
-count = exetime + 30
-
-print "強制終了 は Hit Ctrl-C"
-try:
-    for i in range(0,count):
-        sleep(1) # could use a backward counter to be preeety :)
-    print('正常終了しました。')
-except KeyboardInterrupt:
-    raw_input('終了します Hit Enter:')
-#
-#   ここで終了処理
-#
-   print('強制終了しました。')
-
-ssh.close()
+    #SSHでマネージャ接続
+    ssh=sshconnect(HOST,USER,PASS)
+    command = "echo 8ik,.lo9 |sudo -S "+SCRIPT_PATH+" %s %s" % (filepath,exetime)
+    ssh.exec_command(command)
+    
+    #Ctrl-Cで強制終了 count秒で正常終了
+    count = int(exetime) + 30
+    
+    print "強制終了 は Hit Ctrl-C"
+    try:
+        for i in range(0,count):
+            time.sleep(1) 
+        print('正常終了しました。')
+    except KeyboardInterrupt:
+        raw_input('終了します Hit Enter:')
+    #
+    #   ここで終了処理
+    #
+        print('強制終了しました。')
+    
+    ssh.close()
