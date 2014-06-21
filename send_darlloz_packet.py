af8e2dee2f12a3f19ce2e3de03977d9bc72dde32
petedreams <koukaku95@hotmail.com>
2014-06-21 13:54:50 +0900

    modify

--- send_darlloz_packet.py
+++ send_darlloz_packet.py
@@ -8,14 +8,18 @@
 from scapy.all import *
 import random
 
-IP_DST='192.168.56.1'
+#IP_DST='94.2.172.225'
+IP_DST='94.2.172.225'
+
+#DATA="\x05"
 DATA="\x00\x01\x00\x20"
 #DATA="hello"
+
 SP = random.randint(49152,65535) # srcport
 SEQ = random.randint(0,4294967295) # sequence number
 
 #RSTパケットを送信しない
-rule = "iptables -A OUTPUT -p tcp --tcp-flags RST RST -s 192.168.56.101 -d 192.168.56.1 -j DROP"
+rule = "iptables -A OUTPUT -p tcp --tcp-flags RST RST -s 192.168.56.101 -j DROP"
 os.system(rule)
 
 #syn送信
@@ -34,10 +38,6 @@ packet_data = IP(dst=IP_DST)/TCP(sport=SP,dport=58455,flags='PA',seq=SEQ+1,ack=m
 send(packet_data)
 
 
-#data送信
-packet_data = IP(dst=IP_DST)/TCP(sport=SP,dport=58455,flags='PA',seq=SEQ+1,ack=my_ack)/Raw(load=DATA)
-send(packet_data)
-
 #ルール消去
-rule2 = "iptables -D OUTPUT -p tcp --tcp-flags RST RST -s 192.168.56.101 -d 192.168.56.1 -j DROP"
+rule2 = "iptables -D OUTPUT -p tcp --tcp-flags RST RST -s 192.168.56.101 -j DROP"
 os.system(rule2)
